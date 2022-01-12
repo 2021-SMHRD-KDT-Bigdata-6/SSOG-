89ce25799eec256fbc307f1e2777c33d4658c394
smhrd <smhrd@DESKTOP-5DVBTJN>
2022-01-12 16:21:19 +0900

    테스트전 커밋

--- SsogSpringProject/src/main/java/kr/ssog/web/BoardController.java
+++ SsogSpringProject/src/main/java/kr/ssog/web/BoardController.java
@@ -2,8 +2,10 @@ package kr.ssog.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.ssog.domain.Board;
 import kr.ssog.service.BoardService;
 
 @Controller
