bfa5915c17211bf4d0cd5385b4351af10fa44852
smhrd <smhrd@DESKTOP-5DVBTJN>
2022-01-12 16:57:29 +0900

    테스트전 수정

--- SsogSpringProject/src/main/java/kr/ssog/web/BoardController.java
+++ SsogSpringProject/src/main/java/kr/ssog/web/BoardController.java
@@ -1,11 +1,10 @@
 package kr.ssog.web;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import kr.ssog.domain.Board;
 import kr.ssog.service.BoardService;
 
 @Controller
@@ -33,5 +32,6 @@ public class BoardController {
 	public String b_writeBoard(){
 		return "b_writeBoard";
 	}
+	
 	
 }
