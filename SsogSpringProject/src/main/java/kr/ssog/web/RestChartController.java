3f849da98b6b320c390db31863ecdf062512b176
smhrd <smhrd@DESKTOP-5DVBTJN>
2022-01-20 21:04:43 +0900

    물가정보 페이지 데이터연동 코드 작성/버튼 수정+이벤트 추가

--- SsogSpringProject/src/main/java/kr/ssog/web/RestChartController.java
+++ SsogSpringProject/src/main/java/kr/ssog/web/RestChartController.java
@@ -1,23 +1,31 @@
 package kr.ssog.web;
-import kr.ssog.domain.Board;
 import kr.ssog.domain.Ingredient;
 import kr.ssog.service.IngredientService;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-//@Controller
-//public class RestChartController{
-//	
-//	@Autowired
-//	IngredientService service;
-//	@GetMapping("/priceInfo")
-//	public List<Ingredient> IngredientCateList() {
-//		List<Ingredient> list = service.boardList();
-//		// list > [{ },{ },{ }] (json) > API(Gson api)
-//		return list ; // @Responsebody : 객체를 json으로 변환시켜서 응답을 해라(전처리)
-//	}
-//}
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestChartController{
+	@Autowired
+	IngredientService service;
+	
+	// 카테고리 클릭시 
+	@GetMapping("/priceInfoAjaxTEST")
+	public @ResponseBody List<String> IngreCateList(HttpServletRequest request){
+		String ingre = request.getParameter("ingre");
+		System.out.println(ingre);
+		List<Ingredient> categori = service.CategoriPriceTop(ingre);
+		List<String> list = service.getAllTodayPrice(categori);
+		return list;
+	}
+	
+	// 개별 검색
+	
+}
